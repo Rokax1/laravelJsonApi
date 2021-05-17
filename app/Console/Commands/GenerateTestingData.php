2ff5ee0ff60aa6b1c1a5307ec3a3eb89301fa1c6
Rokax1 <leandrofernando739@gmail.com>
2021-05-17 13:21:33 -0400

    comando seeder para obtener datos de testing

--- app/Console/Commands/GenerateTestingData.php
+++ app/Console/Commands/GenerateTestingData.php
@@ -2,23 +2,28 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Article;
+use App\Models\Category;
+use App\User;
 use Illuminate\Console\Command;
+use Illuminate\Console\ConfirmableTrait;
 
 class GenerateTestingData extends Command
 {
+    use ConfirmableTrait;
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'generate:test-data';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'generate test data for the api!';
 
     /**
      * Create a new command instance.
@@ -37,6 +42,38 @@ class GenerateTestingData extends Command
      */
     public function handle()
     {
-        return 0;
+
+        if (!$this->confirmToProceed()) {
+            return 1;
+        }
+
+
+        User::query()->delete();
+        Article::query()->delete();
+        Category::query()->delete();
+
+       $user= User::factory()->hasArticles(1)->create([
+            'name'=>'leandro',
+            'email'=> 'leandrofernando739@gmail.com'
+        ]);
+
+        $articles = Article::factory()->count(14)->create();
+
+        $this->line("");
+        $this->info('User UUID');
+        $this->line($user->id);
+        $this->line("");
+        $this->info('Token');
+        $this->line($user->createToken('jorge')->plainTextToken);
+        $this->line("");
+        $this->info("Articles Id");
+        $this->line($user->articles->first()->slug);
+        $this->line("");
+        $this->info("Category Id");
+
+        $this->line($articles->first()->category->id);
+
+
+
     }
 }
